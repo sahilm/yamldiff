c574e3cfbbe6f14d09946b6b6a7d4d033206aae3
Sahil Muthoo <sahil.muthoo@gmail.com>
2017-08-04 20:29:38 +0100

    Introduct versioning

--- main.go
+++ main.go
@@ -21,10 +21,21 @@ func main() {
 		File1   string `long:"file1" description:"first YAML file" required:"true"`
 		File2   string `long:"file2" description:"second YAML file" required:"true"`
 		NoColor bool   `long:"no-color" description:"disable colored output" required:"false"`
+		Version func() `long:"version" description:"print version and exit"`
+	}
+
+	var version = "latest"
+
+	opts.Version = func() {
+		fmt.Fprintf(os.Stderr, "%v\n", version)
+		os.Exit(0)
 	}
 
 	_, err := flags.Parse(&opts)
 	if err != nil {
+		if err.(*flags.Error).Type == flags.ErrHelp {
+			os.Exit(0)
+		}
 		os.Exit(1)
 	}
 
